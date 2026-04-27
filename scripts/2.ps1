0a15a79ac7eb2e00c26907867985240a5275a8f4
ketw <ketkushz@gmail.com>
2026-04-28 00:41:16 +0530

    too lazy, copied the flush and apache restart logic from 0.ps1

--- scripts/2.ps1
+++ scripts/2.ps1
@@ -90,3 +90,28 @@ if (Test-Path $vhostsPath) {
         Write-Host "[2.ps1:VHOST-add] added $baseDomain (with www alias)." -ForegroundColor Green
     } else { Write-Host "[2.ps1:VHOST-skip] vhost config for $baseDomain already exists." -ForegroundColor Yellow; }
 } else { Write-Host "[2.ps1:VHOST-error] could not locate httpd-vhosts.conf at: $vhostsPath" -ForegroundColor Red; Exit 1; }
+
+Write-Host "[2.ps1:SYSTEM-dns] flushing dns cache..." -ForegroundColor Yellow
+Clear-DnsClientCache
+Write-Host "[2.ps1:SYSTEM-dns] flush successful" -ForegroundColor Green
+
+Write-Host "[0.ps1:XAMPP-apache] apache restart required, restarting..." -ForegroundColor Cyan
+$ApacheService = Get-Service | Where-Object { $_.Name -like "Apache*" }
+if ($ApacheService) {
+    Write-Host "[0.ps1:XAMPP-apache] found service, restarting..." -ForegroundColor Yellow
+    try {
+        Restart-Service -Name $ApacheService.Name -Force -ErrorAction SilentlyContinue
+        Write-Host "[0.ps1:XAMPP-apache] service restarted." -ForegroundColor Green
+    } catch { Write-Host "[0.ps1:XAMPP-apache] failed to restart service, manually attempting to restart..." -ForegroundColor Yellow; }
+} else {
+    $HttpdPath = Join-Path -Path $XamppInstallDir -ChildPath "apache\bin\httpd.exe"
+    if (Test-Path $HttpdPath) {
+        Write-Host "[0.ps1:XAMPP-apache] service not found, restarting via executable..." -ForegroundColor Yellow
+        Get-Process "httpd" -ErrorAction SilentlyContinue | Stop-Process -Force
+        Start-Sleep -Seconds 1
+        Start-Process -FilePath $HttpdPath -WindowStyle Hidden
+        Write-Host "[0.ps1:XAMPP-apache] process restarted." -ForegroundColor Green
+    } else {
+        Write-Host "[0.ps1:XAMPP-apache] could not locate httpd.exe at $HttpdPath" -ForegroundColor Red
+    }
+}
